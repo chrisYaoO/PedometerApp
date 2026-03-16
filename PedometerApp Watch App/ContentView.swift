d3d773caa75e6e4e6d81b4dda68e534be9c1dc81
chrisYaoO <20307140015@fudan.edu.cn>
2026-03-16 17:27:20 -0400

    Initial Commit

---
+++ PedometerApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PedometerApp Watch App
+//
+//  Created by Chris Yao on 2026-03-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PedometerApp Watch App/PedometerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerAppApp.swift
+//  PedometerApp Watch App
+//
+//  Created by Chris Yao on 2026-03-16.
+//
+
+import SwiftUI
+
+@main
+struct PedometerApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
